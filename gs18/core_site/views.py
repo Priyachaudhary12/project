cc4a11fa13ca78d956d1ff5f3df0ff38d3a2ec32
Priyachaudhary12 <priyachaudhary10121999@gmail.com>
2024-12-21 10:59:48 +0530

    minorchange

---
+++ gs18/core_site/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+  return render(request, 'core_site/home.html')
+
+def about(request):
+  return render(request, 'core_site/about.html', {'abc':'/about'})
